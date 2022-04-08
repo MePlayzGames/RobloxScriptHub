9f0c388ea795b6908ab53986dc33810e6cc65737
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-08 16:30:38 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -2,7 +2,20 @@ print("Running DF Farm")
 warn("Version: 0.0.1")
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Made by MePlayzGames#3667", 
-	Text = "Exploiting is very not cool",
+	Title = "MePlayzGames#3667", 
+	Text = "Exploiting is very not epik",
 	Icon = "rbxassetid://7028540906"
 })
+
+while wait (1) do
+	for i,v in pairs(FruitList) do
+		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit",v)
+	end
+	
+	for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
+		if v:IsA("Tool") then
+			if string.find(v.Name, "Fruit") then
+				print(v.Name)
+			end
+		end
+	end
