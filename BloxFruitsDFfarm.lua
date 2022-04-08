e329bbd5231eab5049f421e9c0f1a63711ee904a
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-08 16:23:07 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -1,7 +1,8 @@
 print("Running DF Farm")
 warn("Version: 0.0.1")
 
-game.StarterGui:SetCore("SendNotification", {
-	Title = "Made by MePlayzGames."
-	Text = "Blox Fruits DF Farm has been executed."
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Made by MePlayzGames#3667", 
+	Text = "Exploiting is very not cool",
+	Icon = "rbxassetid://7028540906"
 })
