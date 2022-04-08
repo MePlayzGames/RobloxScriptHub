579067dea421b6fac6534c5ace45c08b0cbe0585
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-07 21:33:13 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -1,18 +1,15 @@
 local player = game.Players.LocalPlayer
 local TeleportService = game:GetService("TeleportService")
+local FarmInProgress = false
 
 -- if game.PlaceId == 4442272183 then
    for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
       if string.find(v.Name, "Fruit") then  
-         print("Found")
-      else
-         TeleportService:Teleport(game.PlaceId, player)
+         print("Found") 
       end
    end       
 -- end
 
-   game:GetService("Players").LocalPlayer.Idled:connect(function()
-        game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-        wait(1)
-        game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-  end)
+while task.wait(10) do
+   
+end
