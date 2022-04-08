d5c7527576e8e6c02aa19462900d3ec3987a005d
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-07 23:05:27 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -6,14 +6,9 @@ if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid
 
 wait(1)
 
-print("Executed")
+print("Ran")
 
-for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
-	if v:IsA("Tool") then
-		local OldCFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame				
-			game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Handle.CFrame * CFrame.new(0,0,8)
-			v.Handle.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
-			wait(.1)
-			game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = OldCFrame
-	end
-end
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Made by MePlayzGames."
+	Text = "Blox Fruits DF Farm has been executed."
+})
