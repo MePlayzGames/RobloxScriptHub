ab0a8bd1e20d8ecfed67d0021479656ebcca6663
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-07 21:42:21 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -2,14 +2,29 @@ local player = game.Players.LocalPlayer
 local TeleportService = game:GetService("TeleportService")
 local FarmInProgress = false
 
+local HRP = player.Character.PrimaryPart
+
 -- if game.PlaceId == 4442272183 then
    for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
       if string.find(v.Name, "Fruit") then  
-         print("Found") 
+         FarmInProgress = true
+         print("FoundFruit")
+         
+         HRP.Anchored = true
       end
    end       
 -- end
 
-while task.wait(10) do
+while task.wait(10) and not FarmInProgress do
+   local isFruit = false
    
+   for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
+      if string.find(v.Name, "Fruit") then  
+         isFruit = true
+      end
+   end  
+   
+   if not isFruit and FarmInProgress then
+      TeleportService:Teleport(game.PlaceId, player)
+   end
 end
