3dce16fdcc30a9bb8df82805f594841c7c91dece
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-07 22:37:56 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -1,7 +1,3 @@
-
-
-local FarmInProgress = false
-
 if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
 
 repeat wait() until game:GetService("Players")
@@ -10,98 +6,17 @@ if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid
 
 wait(1)
 
-local player = game.Players.LocalPlayer
-local HRP = player.Character.HumanoidRootPart
-
 print("Executed")
 
--- if game.PlaceId == 4442272183 then
-
--- end
-function Hop()
-		local PlaceID = game.PlaceId
-		local AllIDs = {}
-		local foundAnything = ""
-		local actualHour = os.date("!*t").hour
-		local Deleted = false
-		function TPReturner()
-			local Site;
-			if foundAnything == "" then
-				Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
-			else
-				Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-			end
-			local ID = ""
-			if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-				foundAnything = Site.nextPageCursor
-			end
-			local num = 0;
-			for i,v in pairs(Site.data) do
-				local Possible = true
-				ID = tostring(v.id)
-				if tonumber(v.maxPlayers) > tonumber(v.playing) then
-					for _,Existing in pairs(AllIDs) do
-						if num ~= 0 then
-							if ID == tostring(Existing) then
-								Possible = false
-							end
-						else
-							if tonumber(actualHour) ~= tonumber(Existing) then
-								local delFile = pcall(function()
-									AllIDs = {}
-									table.insert(AllIDs, actualHour)
-								end)
-							end
-						end
-						num = num + 1
-					end
-					if Possible == true then
-						table.insert(AllIDs, ID)
-						wait()
-						pcall(function()
-							wait()
-							game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-						end)
-						wait(4)
+pcall(function()
+				for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
+					if v:IsA("Tool") then
+						local OldCFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame				
+						game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Handle.CFrame * CFrame.new(0,0,8)
+						v.Handle.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
+						wait(.1)
+						game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = OldCFrame
 					end
 				end
-			end
-		end
-		function Teleport() 
-			while wait() do
-				pcall(function()
-					TPReturner()
-					if foundAnything ~= "" then
-						TPReturner()
-					end
-				end)
-			end
-		end
-		Teleport()
-	end  
-
-while task.wait(10) and not FarmInProgress do
-	local isFruit = false
-	
-	game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Cousin","Buy")
-
-	for i, v in pairs(game:GetService("Workspace"):GetChildren()) do
-		if string.find(v.Name, "Fruit") then  
-			if v:IsA("Tool") then
-				isFruit = true
-				FarmInProgress = true
-
-				print("FoundFruit")
-
-				HRP.CFrame = v.Handle.CFrame * CFrame.new(0,0,8)
-				
-				game:GetService("Players").LocalPlayer.Character:FindFirstChild(_G.SelectFruitEat).EatRemote:InvokeServer()
-			end
-		end
-	end  
-
-	if not isFruit and not FarmInProgress then
-		print("Could not find fruit")
-		Hop()
 	end
-end
+end)
