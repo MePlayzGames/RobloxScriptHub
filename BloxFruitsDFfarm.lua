a02b59e4fdb553942ce1c3d82ee26587bb5a40bb
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-08 17:05:13 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -14,70 +14,7 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "MePlayzGames#3667", 
 	Text = "Exploiting is very not epik",
 	Icon = "rbxassetid://7028540906"
-})
-
-function Hop()
-	local PlaceID = game.PlaceId
-	local AllIDs = {}
-	local foundAnything = ""
-	local actualHour = os.date("!*t").hour
-	local Deleted = false
-	local function TPReturner()
-		local Site;
-		if foundAnything == "" then
-			Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
-		else
-			Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-		end
-		local ID = ""
-		if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-			foundAnything = Site.nextPageCursor
-		end
-		local num = 0;
-		for i,v in pairs(Site.data) do
-			local Possible = true
-			ID = tostring(v.id)
-			if tonumber(v.maxPlayers) > tonumber(v.playing) then
-				for _,Existing in pairs(AllIDs) do
-					if num ~= 0 then
-						if ID == tostring(Existing) then
-							Possible = false
-						end
-					else
-						if tonumber(actualHour) ~= tonumber(Existing) then
-							local delFile = pcall(function()
-								AllIDs = {}
-								table.insert(AllIDs, actualHour)
-							end)
-						end
-					end
-					num = num + 1
-				end
-				if Possible == true then
-					table.insert(AllIDs, ID)
-					wait()
-					pcall(function()
-						wait()
-						game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-					end)
-					wait(4)
-				end
-			end
-		end
-	end
-
-	local function Teleport() 
-		while wait() do
-			pcall(function()
-				TPReturner()
-				if foundAnything ~= "" then
-					TPReturner()
-				end
-			end)
-		end
-	end
-	Teleport()
-end               
+})             
 
 FruitList = {
 	"Bomb-Bomb",
@@ -135,19 +72,5 @@ while wait (1) and not Teleporting do
 				FarmRunning = false
 			end
 		end
-	end
-
-	if not FarmRunning then
-		print("NoFruit")
-
-		Teleporting = true
-		
-		if game.PlaceId == 4442272183 then
-			for i,v in pairs(FruitList) do 
-				game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit",v)
-			end
-		end
-
-		Hop()
 	end
 end
