3e58ee4f7ae940990c9fccc65b4229a10870f2ab
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-08 16:37:17 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -1,6 +1,12 @@
 print("Running DF Farm")
 warn("Version: 0.0.2")
 
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+
+repeat wait() until game:GetService("Players")
+
+if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") end
+
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "MePlayzGames#3667", 
 	Text = "Exploiting is very not epik",
@@ -8,46 +14,48 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 })
 
 FruitList = {
-		"Bomb-Bomb",
-		"Spike-Spike",
-		"Chop-Chop",
-		"Spring-Spring",
-		"Kilo-Kilo",
-		"Spin-Spin",
-		"Bird: Falcon",
-		"Smoke-Smoke",
-		"Flame-Flame",
-		"Ice-Ice",
-		"Sand-Sand",
-		"Dark-Dark",
-		"Revive-Revive",
-		"Diamond-Diamond",
-		"Light-Light",
-		"Love-Love",
-		"Rubber-Rubber",
-		"Barrier-Barrier",
-		"Magma-Magma",
-		"Door-Door",
-		"Quake-Quake",
-		"Human-Human: Buddha",
-		"String-String",
-		"Bird-Bird: Phoenix",
-		"Rumble-Rumble",
-		"Paw-Paw",
-		"Gravity-Gravity",
-		"Dough-Dough",
-		"Venom-Venom",
-		"Shadow-Shadow",
-		"Control-Control",
-		"Soul-Soul",
-		"Dragon-Dragon"
-	}
+	"Bomb-Bomb",
+	"Spike-Spike",
+	"Chop-Chop",
+	"Spring-Spring",
+	"Kilo-Kilo",
+	"Spin-Spin",
+	"Bird: Falcon",
+	"Smoke-Smoke",
+	"Flame-Flame",
+	"Ice-Ice",
+	"Sand-Sand",
+	"Dark-Dark",
+	"Revive-Revive",
+	"Diamond-Diamond",
+	"Light-Light",
+	"Love-Love",
+	"Rubber-Rubber",
+	"Barrier-Barrier",
+	"Magma-Magma",
+	"Door-Door",
+	"Quake-Quake",
+	"Human-Human: Buddha",
+	"String-String",
+	"Bird-Bird: Phoenix",
+	"Rumble-Rumble",
+	"Paw-Paw",
+	"Gravity-Gravity",
+	"Dough-Dough",
+	"Venom-Venom",
+	"Shadow-Shadow",
+	"Control-Control",
+	"Soul-Soul",
+	"Dragon-Dragon"
+}
 
 while wait (1) do
-	for i,v in pairs(FruitList) and game.PlaceID == 4442272183 do
+	--[[
+	for i,v in pairs(FruitList) and game.PlaceID == 4442272183 do 
 		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit",v)
 	end
-	
+	]]
+
 	for i,v in pairs(game:GetService("Workspace"):GetChildren()) do
 		if v:IsA("Tool") then
 			if string.find(v.Name, "Fruit") then
