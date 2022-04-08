a99cda849b6aad5c75b5175b8401f1ae7297d966
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-07 23:10:47 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -1,7 +1,5 @@
 if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
 
-repeat wait() until game:GetService("Players")
-
 if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") end
 
 wait(1)
