0a6011aa58cb05dfc3bcc3ec89166882ea2a40d2
MePlayzGames <68137143+MePlayzGames@users.noreply.github.com>
2022-04-08 16:34:06 -0600

    Update BloxFruitsDFfarm.lua

--- BloxFruitsDFfarm.lua
+++ BloxFruitsDFfarm.lua
@@ -7,6 +7,42 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 	Icon = "rbxassetid://7028540906"
 })
 
+FruitList = {
+		"Bomb-Bomb",
+		"Spike-Spike",
+		"Chop-Chop",
+		"Spring-Spring",
+		"Kilo-Kilo",
+		"Spin-Spin",
+		"Bird: Falcon",
+		"Smoke-Smoke",
+		"Flame-Flame",
+		"Ice-Ice",
+		"Sand-Sand",
+		"Dark-Dark",
+		"Revive-Revive",
+		"Diamond-Diamond",
+		"Light-Light",
+		"Love-Love",
+		"Rubber-Rubber",
+		"Barrier-Barrier",
+		"Magma-Magma",
+		"Door-Door",
+		"Quake-Quake",
+		"Human-Human: Buddha",
+		"String-String",
+		"Bird-Bird: Phoenix",
+		"Rumble-Rumble",
+		"Paw-Paw",
+		"Gravity-Gravity",
+		"Dough-Dough",
+		"Venom-Venom",
+		"Shadow-Shadow",
+		"Control-Control",
+		"Soul-Soul",
+		"Dragon-Dragon"
+	}
+
 while wait (1) do
 	for i,v in pairs(FruitList) and game.PlaceID == 4442272183 do
 		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit",v)
